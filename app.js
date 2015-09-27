b8d78c77affe6cc4a94cef94051ef8ab54e477c6
Siripong Tianpajeekul <siripong.t@sawasdee-idea.com>
2015-09-27 21:31:58 +0700

    Test Deploy on Modulus

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+        app = express();
+
+app.get('/', function(req, res) {
+  console.log('Request received: ' + req.url);
+  res.send('Hello World!');
+});
+
+
+app.listen('8080');
+console.log('Modulus demo app started on port 8080');
